e70439ce11595083e5da22a9da5e826fce4f2a91
pc-mist <mistsxd@gmail.com>
2016-01-30 02:40:08 +0700

    Dashboard. Reformat templates

--- dashboard/app/dashboard/views.py
+++ dashboard/app/dashboard/views.py
@@ -2,4 +2,4 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'dashboard/index.html')
+    return render(request, 'base/index.html')
