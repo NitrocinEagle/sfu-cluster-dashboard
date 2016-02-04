199f420e1c786dde20426d5288433f25293bf714
pc-mist <mist_sx@mail.ru>
2016-02-04 15:50:09 +0700

    changed models

--- dashboard/app/models.py
+++ dashboard/app/models.py
@@ -44,38 +44,20 @@ class ParamDescription(EmbeddedDocument):
     axis_x_title = StringField(max_length=20, default="Time line")
     graph_type = StringField(max_length=30)
 
-"""
-В параметрах в зависимости от способа отображения могут хранится:
-1. Название параметра;
-2. Метка для оси X;
-3. Метка для оси Y;
-4. Способ отображения данного параметра;
-5. Набор данных, который нужно отображать.
 
-В наборе данных содержатся данные в формате, требуемом от способа отображения.
-"""
 class ParamData(EmbeddedDocument):
     param_description = EmbeddedDocumentField(ParamDescription)
     dataset = DynamicField(default=LineChartMetric)
 
 
-"""
-Описание формата собранных данных для конкретного узла. Собранные даные разбиты на фрагменты по плагинам.
-В каждом фрагменте содержится:
-1. Название плагина;
-2. Параметры.
-"""
 class PluginData(EmbeddedDocument):
     plugin_name = StringField(max_length=30)
     param_data_list = EmbeddedDocumentListField(ParamData)
 
-"""
-Здесь хранятся данные мониторинга для каждого узла. В каждом наборе данных определены:
-1. Имя узла;
-2. Ip-адрес узла;
-3. Собранные данные. (формат собранных данных описан выше)
-"""
+
 class NodeData(Document):
     node_name = StringField(max_length=50)
     node_ip = StringField(max_length=15)
-    plugin_data_list = EmbeddedDocumentListField(PluginData)
+    plugin_name = StringField(max_length=30)
+    param_name = StringField(max_length=30)
+    data = DynamicField(default=LineChartMetric)
