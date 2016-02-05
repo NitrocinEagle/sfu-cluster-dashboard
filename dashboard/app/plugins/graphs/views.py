142c915dc26a5dc323bb588db0b5e27e4489729a
pc-mist <mistsxd@gmail.com>
2016-02-05 20:40:18 +0700

    Issue 8..

--- dashboard/app/plugins/graphs/views.py
+++ dashboard/app/plugins/graphs/views.py
@@ -12,28 +12,24 @@ class GraphsPluginsView(View):
     template_name = 'graphs/graphs.html'
 
     def get(self, request, *args, **kwargs):
-        # uncomment it if there is workable DB
-        """
         graphs_info = []
-        plugin_name = kwargs.get('plugin_name')  # TODO: add 'try ... except'
-        plugin_params = [param.param_name for param in PluginInfo.objects(
-                plugin_name=plugin_name).params_info]
+        plugin_name = kwargs.get('plugin_name')
+        # TODO: add 'try ... except'
+        plugin_params = [param.param_name for param in PluginInfo.objects(plugin_name=plugin_name)[0].params_info]
         nodes = NodeInfo.objects()
         id = 1
         for node in nodes:
             if plugin_name in node.enabled_plugins:
                 for param in plugin_params:
                     graphs_info.append(
-                            {
-                                "id": "graph_id_" + str(id),
-                                "plugin_name": plugin_name,
-                                "node_name": node.node_name,
-                                "param_name": param
-                            }
+                        {
+                            "id": "graph_id_" + str(id),
+                            "plugin_name": plugin_name,
+                            "node_name": node.node_name,
+                            "param_name": param
+                        }
                     )
                     id += 1
-        """
-        # uncomment it if there is workable DB
         test_graphs_info = [
             {
                 "id": "graph_id_1",
@@ -54,6 +50,5 @@ class GraphsPluginsView(View):
                 "param_name": "cpu_load"
             }
         ]
-        return render(request, self.template_name,{'graphs_info': test_graphs_info})
-        # uncomment it if there is workable DB
-        #return render(request, self.template_name,{'graphs_info': graphs_info})
+        # return render(request, self.template_name,{'graphs_info': test_graphs_info})
+        return render(request, self.template_name, {'graphs_info': graphs_info})
