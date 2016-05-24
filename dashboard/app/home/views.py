a52429f1c6f1443e86158c2ae4e50e443a8a787a
pc-mist <mist_sx@mail.ru>
2016-05-24 19:02:28 +0700

    Graphs reawaken

--- dashboard/app/home/views.py
+++ dashboard/app/home/views.py
@@ -13,7 +13,7 @@ from ..mongo_models import ServerInfo, PluginInfo, NodeInfo
 class LoginFormView(FormView):
     form_class = AuthenticationForm
 
-    template_name = "index/login.html"
+    template_name = "site/index/login.html"
     success_url = "/"
 
     def form_valid(self, form):
@@ -33,63 +33,85 @@ class DashboardView(TemplateView):
     template_name = 'site/index/new_dashboard.html'
 
     def get_context_data(self, **kwargs):
-        kwargs['server_info'] = ServerInfo.objects.first().info
+        # kwargs['server_info'] = ServerInfo.objects.first().info
+        kwargs['server_info'] = [{
+            'name': 'name',
+            'value': '11',
+            'fa_icon': 'fa-gear',
+            'label': 'label'
+        }, {
+            'name': 'name',
+            'value': '11',
+            'fa_icon': 'fa-gear',
+            'label': 'label'
+        }, {
+            'name': 'name',
+            'value': '11',
+            'fa_icon': 'fa-gear',
+            'label': 'label'
+        }, {
+            'name': 'name',
+            'value': '11',
+            'fa_icon': 'fa-gear',
+            'label': 'label'
+        }]
+
         kwargs['monitoring_states'] = [{
-                'label': '2',
-                'fa_icon': 'fa-server',
-                'name': u'Мониторинг серверов',
-                'url': '/',
-                'value': u'Danger'
-            }, {
-                'label': '2',
-                'fa_icon': 'fa-users',
-                'name': u'Мониторинг пользователей',
-                'url': '/',
-                'value': u'Normal'
-            }, {
-                'label': '2',
-                'fa_icon': 'fa-gear',
-                'name': u'Мониторинг инженерных параметров',
-                'url': '/',
-                'value': u'Warning'
-            },
+            'label': '2',
+            'fa_icon': 'fa-server',
+            'name': u'Мониторинг серверов',
+            'url': '/',
+            'value': u'Danger'
+        }, {
+            'label': '2',
+            'fa_icon': 'fa-users',
+            'name': u'Мониторинг пользователей',
+            'url': '/',
+            'value': u'Normal'
+        }, {
+            'label': '2',
+            'fa_icon': 'fa-gear',
+            'name': u'Мониторинг инженерных параметров',
+            'url': '/',
+            'value': u'Warning'
+        },
         ]
         kwargs['notifications'] = [{
-                'id': 1,
-                'type': 'critical',
-                'text': u'Случилось что-то страшное',
-                'time': '12:43 12.09.2016',
-            }, {
-                'id': 1,
-                'type': 'critical',
-                'text': u'Случилось что-то страшное',
-                'time': '12:43 12.09.2016',
-            },{
-                'id': 1,
-                'type': 'critical',
-                'text': u'Случилось что-то страшное',
-                'time': '12:43 12.09.2016',
-            }, {
-                'id': 2,
-                'type': 'config',
-                'text': u'Изменен таймаут параметра А в узле Б',
-                'time': '12:43 12.09.2016',
-            }, {
-                'id': 3,
-                'type': 'info',
-                'text': u'Добавлен узел С',
-                'time': '12:43 12.09.2016',
-            }, {
-                'id': 3,
-                'type': 'critical',
-                'text': u'Случилось что-то страшное',
-                'time': '12:43 12.09.2016',
-            }, {
-                'id': 4,
-                'type': 'warning',
-                'text': u'Параметр В выше нормы',
-                'time': '12:43 12.09.2016',
-            },
+            'id': 1,
+            'type': 'critical',
+            'text': u'Случилось что-то страшное',
+            'time': '12:43 12.09.2016',
+        }, {
+            'id': 1,
+            'type': 'critical',
+            'text': u'Случилось что-то страшное',
+            'time': '12:43 12.09.2016',
+        }, {
+            'id': 1,
+            'type': 'critical',
+            'text': u'Случилось что-то страшное',
+            'time': '12:43 12.09.2016',
+        }, {
+            'id': 2,
+            'type': 'config',
+            'text': u'Изменен таймаут параметра А в узле Б',
+            'time': '12:43 12.09.2016',
+        }, {
+            'id': 3,
+            'type': 'info',
+            'text': u'Добавлен узел С',
+            'time': '12:43 12.09.2016',
+        }, {
+            'id': 3,
+            'type': 'critical',
+            'text': u'Случилось что-то страшное',
+            'time': '12:43 12.09.2016',
+        }, {
+            'id': 4,
+            'type': 'warning',
+            'text': u'Параметр В выше нормы',
+            'time': '12:43 12.09.2016',
+        },
         ]
         kwargs['plugins'] = PluginInfo.objects()
         kwargs['nodes'] = NodeInfo.objects()
--- dashboard/conf/settings.py
+++ dashboard/conf/settings.py
@@ -33,7 +33,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'static_precompiler',
     'app.user',
 ]
 
